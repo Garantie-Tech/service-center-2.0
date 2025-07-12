96f0e63107bd2525a2c51b2729db55746463d733
Ashish-Garantie <Ashish@garantie.in>
2025-07-12 13:02:23 +0530

    compress images if size if greater than 2 mb

--- src/utils/compressImage.ts
+++ src/utils/compressImage.ts
@@ -5,6 +5,13 @@ export const compressImage = async (
   maxSizeMB = 1,
   maxWidthOrHeight = 1024
 ): Promise<File> => {
+  const sizeInMB = file.size / (1024 * 1024); // Convert bytes to MB
+
+  if (sizeInMB <= 2) {
+    // No need to compress
+    return file;
+  }
+
   const options = {
     maxSizeMB,
     maxWidthOrHeight,
